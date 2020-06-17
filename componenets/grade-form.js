16a341132b765623c7ca110d3e5c9188f5c2add7
Jenna Bowman <jbowman1@swarthmore.edu>
2020-06-17 19:02:06 +0000

    Completed add grade.

---
+++ componenets/grade-form.js
@@ -0,0 +1,19 @@
+class GradeForm {
+  constructor(formElement) {
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement = formElement;
+    this.formElement.addEventListener('submit', this.handleSubmit);
+  }
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    var newName = formData.get("name");
+    var newCourse = formData.get("course");
+    var newGrade = formData.get("grade");
+    this.createGrade(newName, newCourse, newGrade);
+    event.target.reset();
+  }
+}
